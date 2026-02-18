0da241c27329f014364e0f2bfe812699697a6fc7
apple <apple@apples-MacBook-Pro.local>
2026-02-19 01:43:45 +0800

    redone auth middleware and usermodel to accomodate register and login for controller

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -1,17 +1,12 @@
-require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = process.env.JWT_SECRET;
 
-const JWT_SECRET = 'process.env.JWT_SECRET';
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split (' ')[1];
 
-const protect = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Unauthorized: No token provided' });
-    }
-
-    const token = authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'No token provided'});
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
@@ -22,4 +17,4 @@ const protect = (req, res, next) => {
     }
 };
 
-module.exports = { protect, JWT_SECRET };
+module.exports = { verifyToken };
