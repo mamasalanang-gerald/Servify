5eb60b91dfb2e2125c12f6e158146ea8f2d28c52
Kaiserxgh <callu0053@gmail.com>
2026-02-20 13:19:45 +0800

    login, dashboard, services, landing page frontend

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import LoginBox from '../components/LoginBox';
+import './styles/login.css';
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <Navbar />
+      <main className="login-main">
+        <LoginBox />
+      </main>
+    </div>
+  );
+};
+
+export default LoginPage;
