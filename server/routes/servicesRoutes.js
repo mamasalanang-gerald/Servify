7359afdec79da68cef78d807d0dab4fc90c94149
mamasalanang-gerald <geraldkasan163@gmail.com>
2026-02-20 19:45:25 +0800

    fix service routes

--- server/routes/servicesRoutes.js
+++ server/routes/servicesRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getServices, getServicesbyId } = require('../controllers/servicesController');
+const { getServices, getServicesbyId, createServices, editServices, removeService } = require('../controllers/servicesController');
 
 router.get('/', getServices);
 router.get('/:id', getServicesbyId);
 
 router.post('/create', createServices);
 router.put('/edit/:id', editServices);
+router.delete('/:id', removeService);
 
 module.exports = router;
